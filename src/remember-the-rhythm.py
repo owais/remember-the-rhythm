0322e7aebbbc5b6c05475bb7dc824a5964e842b0
Owais Lone <loneowais@gmail.com>
2011-11-11 23:27:22 +0530

    Bug Fix: Sometimes fails to remember playtime time

--- src/remember-the-rhythm.py
+++ src/remember-the-rhythm.py
@@ -15,11 +15,14 @@ class RememberTheRhythm(GObject.Object, Peas.Activatable):
     object = GObject.property(type=GObject.Object)
     location = None
     playback_time = None
+    first_run = False
 
     def __init__(self):
         GObject.Object.__init__(self)
         try:
-            config_data = open(CONFIG_FILE, 'r').read()
+            config_file = open(CONFIG_FILE, 'r')
+            config_data = config_file.read()
+            config_file.close()
         except IOError:
             return
         config_data = config_data.split()
@@ -30,9 +33,9 @@ class RememberTheRhythm(GObject.Object, Peas.Activatable):
         self.shell = self.object
         self.shell_player = self.shell.props.shell_player
         self.db = self.shell.props.db
-        self.shell.connect('database-load-complete', self.load_complete)
         self.shell_player.connect('elapsed-changed', self.elapsed_changed)
         self.shell_player.connect('playing-song-changed', self.playing_changed)
+        self.shell.connect('database-load-complete', self.load_complete)
 
     def do_deactivate(self):
         self.save_rhythm()
@@ -42,19 +45,25 @@ class RememberTheRhythm(GObject.Object, Peas.Activatable):
             entry = self.db.entry_lookup_by_location(self.location)
             source = self.shell.guess_source_for_uri(self.location)
             self.shell_player.play_entry(entry, source)
-            if self.playback_time:
-                self.shell_player.set_playing_time(long(self.playback_time))
+            self.first_run = True
 
-    def save_rhythm(self):
-        config_file = open(CONFIG_FILE, 'w')
-        config_data = '\n'.join([self.location, self.playback_time])
-        config_file.write(config_data)
-        config_file.close()
+    def save_rhythm(self, pb_time=None):
+        if self.location:
+            pb_time = pb_time == None and self.playback_time or pb_time
+            config_file = open(CONFIG_FILE, 'w')
+            config_data = '\n'.join([self.location, pb_time])
+            config_file.write(config_data)
+            config_file.close()
 
     def playing_changed(self, player, entry, data=None):
+        if self.first_run and self.playback_time:
+            self.shell_player.set_playing_time(long(self.playback_time))
+            self.first_run = False
         try:
-            self.location = str(entry.get_string(RB.RhythmDBPropType.LOCATION))
-            self.save_rhythm()
+            new_location = str(entry.get_string(RB.RhythmDBPropType.LOCATION))
+            if not new_location == self.location:
+                GObject.idle_add(self.save_rhythm, '0')
+            self.location = new_location
         except:
             pass
 
